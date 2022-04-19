3135547d1ec7d7302b3efc4ef4e5547b926354ca
MatiasJoaquinReynoso <matiasjoaquinreynoso@gmail.com>
2022-04-19 19:47:35 -0300

    BaSP-W05-2022: ex06_functions.js

--- Semana-05/exercises/05_for.js
+++ Semana-05/exercises/05_for.js
@@ -5,7 +5,7 @@ console.log('--EXERCISE 5: FOR');
 
 console.log('Exercise 5.a:');
 
-var food = [' pear ',' banana ',' carrot ',' tomato ',' ettuce '];
+var food = [' pear ',' banana ',' carrot ',' tomato ',' lettuce '];
 
 for (var x = 0; x < 5 ; x++) {
 	alert(food[x]);
